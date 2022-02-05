213a5a4b46498f9d164bf866d3065236ac5e7f5b
jiseok20 <colboy0273@gmail.com>
2022-02-06 01:45:47 +0900

    apply MLKIT 2

---
+++ TEST4/app/src/main/java/com/example/test4/MLKIT/LivePreviewPreferenceFragment.java
@@ -0,0 +1,7 @@
+package com.example.test4.MLKIT;
+
+import android.preference.PreferenceFragment;
+
+public abstract class LivePreviewPreferenceFragment extends PreferenceFragment {
+    abstract void setUpCameraPreferences();
+}
---
+++ TEST4/app/src/main/java/com/example/test4/MLKIT/SettingsActivity.java
@@ -0,0 +1,56 @@
+package com.example.test4.MLKIT;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.example.test4.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    public static final String EXTRA_LAUNCH_SOURCE = "extra_launch_source";
+
+    /** Specifies where this activity is launched from. */
+    @SuppressWarnings("NewApi") // CameraX is only available on API 21+
+    public enum LaunchSource {
+        LIVE_PREVIEW(R.string.pref_screen_title_live_preview, LivePreviewPreferenceFragment.class),
+        CAMERAX_LIVE_PREVIEW(
+                R.string.pref_screen_title_camerax_live_preview,
+                CameraXLivePreviewPreferenceFragment.class);
+
+        private final int titleResId;
+        private final Class<? extends PreferenceFragment> prefFragmentClass;
+
+        LaunchSource(int titleResId, Class<? extends PreferenceFragment> prefFragmentClass) {
+            this.titleResId = titleResId;
+            this.prefFragmentClass = prefFragmentClass;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_settings);
+
+        LaunchSource launchSource =
+                (LaunchSource) getIntent().getSerializableExtra(EXTRA_LAUNCH_SOURCE);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(launchSource.titleResId);
+        }
+
+        try {
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(
+                            R.id.settings_container,
+                            launchSource.prefFragmentClass.getDeclaredConstructor().newInstance())
+                    .commit();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
