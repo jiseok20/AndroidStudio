169f932f7101880bb430b92563991249c51a55bc
jiseok20 <colboy0273@gmail.com>
2022-02-09 16:28:39 +0900

    기초디자인

---
+++ TEST4/app/src/main/java/com/example/test4/CreditActivity.java
@@ -0,0 +1,25 @@
+package com.example.test4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CreditActivity extends AppCompatActivity {
+    private String str1,str2;
+    private TextView makers;
+    private TextView git_link;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit);
+
+        makers=findViewById(R.id.text_makers);
+        git_link=findViewById(R.id.git_link);
+        str1 = "Access Management \n\n\n2017035217 이지석\n2017035136 장준호\n";
+        str2 = "https://github.com/jiseok20/AndroidStudio/tree/master";
+        makers.setText(str1);
+        git_link.setText(str2);
+
+    }
+}
--- TEST4/app/src/main/java/com/example/test4/MainActivity.java
+++ TEST4/app/src/main/java/com/example/test4/MainActivity.java
@@ -1,46 +1,25 @@
 package com.example.test4;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.test4.MLKIT.CameraXLivePreviewActivity;
+import com.bumptech.glide.Glide;
+
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText name;
-    private EditText grade_number;
-    private String str1,str2;
-    private Button btn_login;
-    private Button btn_webview;
-    private ListView list_string;
-    private Button btn_start,btn_stop;
-    private Button btn_music_start,btn_music_stop;
-    private Spinner spinner;
-    private Button btn_QR;
-    private TextView array_result;
+    private Button btn_QR,btn_join,btn_search,btn_covid;
     private long backBtnTime=0;
-
     ImageView btn_img;
-
-    Thread thread;
     boolean isThread = false;
 
     @Override
@@ -62,106 +41,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = findViewById(R.id.name);
-        grade_number = findViewById(R.id.grade_number);
-        btn_login=findViewById(R.id.btn_login);
-        list_string =(ListView) findViewById(R.id.list_string);
-        btn_img = (ImageView)findViewById(R.id.btn_img);
-        btn_webview =findViewById(R.id.btn_webview);
-        btn_start=findViewById(R.id.btn_start);
-        btn_stop=findViewById(R.id.btn_stop);
-        btn_music_start=findViewById(R.id.btn_music_start);
-        btn_music_stop=findViewById(R.id.btn_music_stop);
-        spinner=findViewById(R.id.spinner);
-        array_result=findViewById(R.id.array_result);
-        btn_QR=findViewById(R.id.btn_QR);
+        btn_img = findViewById(R.id.gif_image);
+        Glide.with(this).load(R.drawable.cancer).into(btn_img);
 
-
-
-
-
-
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                str1 = name.getText().toString();
-                str2=grade_number.getText().toString();
-                Intent intent = new Intent(MainActivity.this , SubActivity.class);
-                intent.putExtra("name",str1);
-                intent.putExtra("grade_number",str2);
-                startActivity(intent); //activity 이동
-
-            }
-        });
-
-        btn_webview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,WebActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        btn_QR=findViewById(R.id.btn_QR); //출입
+        btn_join=findViewById(R.id.btn_join); //등록
+        btn_search=findViewById(R.id.btn_search); // 조회
+        btn_covid=findViewById(R.id.btn_covid); //확진
 
         btn_img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"훠훠훠",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        //thread start
-        btn_start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isThread = true;
-                thread = new Thread(){
-                  public void run(){
-                      while (isThread){
-                          try {
-                              sleep(3000);
-                          } catch (InterruptedException e) {
-                              e.printStackTrace();
-                          }
-                          handler.sendEmptyMessage(0);
-                      }
-                  }
-                };
-                thread.start();
-            }
-        });
-        //thread stop
-        btn_stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isThread = false;
-            }
-        });
-
-
-        List<String> data = new ArrayList<>();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
-        list_string.setAdapter(adapter);
-
-        data.add("ListView");
-        data.add("졸작 얼른 만들고 싶다");
-        data.add("스레드 버튼을 눌러보세요!");
-        adapter.notifyDataSetChanged();
-
-        btn_music_start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startService(new Intent(getApplicationContext(),MusicService.class));
-
-            }
-        });
-
-        btn_music_stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                stopService(new Intent(getApplicationContext(),MusicService.class));
-
-
+                Intent intent = new Intent(MainActivity.this,CreditActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -173,31 +65,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                array_result.setText(adapterView.getItemAtPosition(i).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-
-
     }
-
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            Toast.makeText(getApplicationContext(),"선주 천재",Toast.LENGTH_SHORT).show();
-            Toast.makeText(getApplicationContext(),"선주 똑똑해",Toast.LENGTH_SHORT).show();
-        }
-    };
-
 }
